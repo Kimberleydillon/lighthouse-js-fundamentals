de8129460c5c1511ebcd5890110fecb80dc93f4e
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-09-09 19:58:41 +0000

    Lint checks corrected, spacing between operators were not exact

--- age_calculator.js
+++ age_calculator.js
@@ -1,5 +1,6 @@
 var ageCalculator = function (name, yearOfBirth, currentYear) {
   var age = currentYear - yearOfBirth;
-  return name + " is "+ age + " years old.";
+  return name + " is " + age + " years old.";
 }
 console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
