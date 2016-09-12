14e097ab17f4e7baddb1a17cef86656d73638467
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-09-12 19:06:55 +0000

    adding ingredients exercise which has for and while loops.

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for ( var i = 0; i < ingredients.length; i++){
+   console.log(ingredients[i])
+}
+
+repeat = 0;
+while (repeat < 1) {
+   console.log(ingredients);
+   repeat++;
+}
+
+
+
+for ( var i = ingredients.length; i >= 0 ; i--) {
+  console.log(ingredients[i]);
+ }
+
